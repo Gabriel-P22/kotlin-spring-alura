f154cdbffc2bba8fbd2f8b46a690744e0dd08fd8
Gabriel <paschoalsoftwareengineer@gmail.com>
2024-11-28 18:50:06 -0300

    feature: add bean validation

--- src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
@@ -3,6 +3,7 @@ package br.com.alura.forum.controller
 import br.com.alura.forum.dto.TopicForm
 import br.com.alura.forum.dto.TopicView
 import br.com.alura.forum.service.TopicService
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -27,7 +28,7 @@ class TopicController(
     }
 
     @PostMapping()
-    fun createTopic(@RequestBody topic: TopicForm) {
+    fun createTopic(@RequestBody @Valid topic: TopicForm) {
         service.create(topic)
     }
 
--- src/main/kotlin/br/com/alura/forum/dto/TopicForm.kt
+++ src/main/kotlin/br/com/alura/forum/dto/TopicForm.kt
@@ -1,11 +1,14 @@
 package br.com.alura.forum.dto
 
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+
 data class TopicForm(
-    var id: Long? = null,
-    val title: String,
-    val message: String,
-    val idCourse: Long,
-    val idUser: Long,
+    @field: NotEmpty @field:Size(min = 5, max = 100) val title: String,
+    @field: NotEmpty val message: String,
+    @field: NotNull val idCourse: Long,
+    @field: NotNull val idUser: Long,
     ) {
 
 }
