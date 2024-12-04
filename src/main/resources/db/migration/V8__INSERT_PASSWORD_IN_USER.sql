d03f731bd5399611578cd7e1edcdae8beb771f68
Gabriel <paschoalsoftwareengineer@gmail.com>
2024-12-04 17:30:45 -0300

    feature: add flyway mysql driver, mysql drivers and fix migrations

--- src/main/resources/db/migration/V8__INSERT_PASSWORD_IN_USER.sql
+++ src/main/resources/db/migration/V8__INSERT_PASSWORD_IN_USER.sql
@@ -1,1 +1,1 @@
-UPDATE users SET password = '$2a$12$NOe6Wtc6exyE7VNQa4Dz.uaoT.q55i4WfNIvMzMOAmq.3C2IH4G0a' WHERE id = 1;
+UPDATE USERS SET password = '$2a$12$NOe6Wtc6exyE7VNQa4Dz.uaoT.q55i4WfNIvMzMOAmq.3C2IH4G0a' WHERE id = 1;
